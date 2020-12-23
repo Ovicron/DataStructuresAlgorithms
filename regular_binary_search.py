aa542f87cec586b4d61ec7f51c155ef2aa2b0147
Ovicron <40534384+Ovicron@users.noreply.github.com>
2020-12-22 20:58:17 -0500

    Update regular_binary_search.py

--- regular_binary_search.py
+++ regular_binary_search.py
@@ -4,7 +4,7 @@ def search(self, nums: List[int], target: int) -> int:
     low = 0
     high = len(nums) - 1
     while low <= high:
-        mid = (low + high) // 2
+        mid = low + (high - low) // 2
 
         if target == nums[mid]:
             return mid

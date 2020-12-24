0f73032cf77d5cdd2b6ec243ecba33f43b76bd6d
Ovicron <40534384+Ovicron@users.noreply.github.com>
2020-12-24 17:48:50 -0500

    Create stack_data_structure.py

---
+++ stack_data_structure.py
@@ -0,0 +1,34 @@
+# Stack data structure
+# Last in first out
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return self.items == []
+
+    def __len__(self):
+        return len(self.items)
+
+
+s = Stack()
+
+s.push(123)
+s.push('abc')
+s.push('xyz')
+s.push(789)
+
+print(s.items)
+print(s.is_empty())
+print(s.peek())

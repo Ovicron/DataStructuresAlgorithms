3dc0454e3b322d9d7c2875bd49863f004c6000e2
Ovicron <40534384+Ovicron@users.noreply.github.com>
2020-12-27 15:57:45 -0500

    Update reverse_linked_list.py

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,9 +1,9 @@
-    def reverse_list(self):
-        prev = None
-        curr = self.head
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-        self.head = prev
+def reverse_list(self):
+    prev = None
+    curr = self.head
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    self.head = prev

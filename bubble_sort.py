21ce36d0677b862cbd9f5a3109138a8d3f90b8ed
Ovicron <40534384+Ovicron@users.noreply.github.com>
2020-12-27 21:00:13 -0500

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+# Time Complexity O(n^2)
+
+def sort(lst):
+    for i in range(len(lst)):
+        for j in range(0, len(lst) - i - 1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+        
+    return lst

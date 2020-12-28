a8807c009138f00d00ea9448d952ecc04781eff5
Ovicron <40534384+Ovicron@users.noreply.github.com>
2020-12-27 20:59:34 -0500

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,10 @@
+# Time Complexiting O(n^2)
+
+def sort(lst):
+    for i in range(len(lst)):
+        min_index = i
+        for j in range(i+1, len(lst)):
+            if lst[min_index] > lst[j]:
+                min_index = j
+        lst[i], lst[min_index] = lst[min_index], lst[i]
+    return lst

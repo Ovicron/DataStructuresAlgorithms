f3fd05d665148444e8e84a233bc5ccb1ade5279b
Ovicron <40534384+Ovicron@users.noreply.github.com>
2020-12-31 20:08:41 -0500

    Update height_and_size_binary_tree.py

--- height_and_size_binary_tree.py
+++ height_and_size_binary_tree.py
@@ -5,3 +5,20 @@ def height(self, node):
     left_height = self.height(node.left)
     right_height = self.height(node.right)
     return 1 + max(left_height, right_height)
+
+# Using stack to keep track of each node count.
+def size(self):
+    if self.root is None:
+        return 0
+
+    stack = Stack()
+    stack.push(self.root)
+    count = 0
+    while len(stack) > 0:
+        node = stack.pop()
+        if node.left:
+            stack.push(node.left)
+        if node.right:
+            stack.push(node.right)
+        count += 1
+    return count

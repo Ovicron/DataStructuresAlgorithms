adceaecde03d07973bcbc22ead4938026e0da207
Ovicron <40534384+Ovicron@users.noreply.github.com>
2021-01-21 16:16:04 -0500

    Create tower_of_hanoi.py

---
+++ Stack/tower_of_hanoi.py
@@ -0,0 +1,14 @@
+def moveTower(height,fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1,fromPole,withPole,toPole)
+        moveDisk(fromPole,toPole)
+        moveTower(height-1,withPole,toPole,fromPole)
+
+counter = 0
+def moveDisk(fp,tp):
+    global c
+    c += 1
+    print("moving disk from",fp,"to",tp)
+
+moveTower(8,"A","B","C")
+print(counter)

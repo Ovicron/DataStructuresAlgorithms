3ba711fafbb1fd5c78655ded3fd1cbfb9b45d75a
Ovicron <40534384+Ovicron@users.noreply.github.com>
2021-01-29 17:47:21 -0500

    Update reverse_list_recursive.py

--- Recursive Algorithms/reverse_list_recursive.py
+++ Recursive Algorithms/reverse_list_recursive.py
@@ -5,5 +5,5 @@ def reverse_list(lst):
   return [lst[-1] + reverse_list(lst[:-1])
   
  
- x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
- print(reverse_list(x))
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(reverse_list(x))
